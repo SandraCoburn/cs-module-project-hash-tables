f214326ac820671174404f9d739eaf26be75dbf1
Sandra Coburn <sandra_coburn@hotmail.com>
2020-08-05 18:44:14 -0700

    No dups finished

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,5 +1,24 @@
+'''
+Input: a string of words separated by spaces. Only the letters a-z are utilized.
+
+Output: the string in the same order, but with subsequent duplicate words removed.
+
+There must be no extra spaces at the end of your returned string.
+
+The solution must be O(n).
+'''
 def no_dups(s):
-    # Your code here
+    cache = {}
+    new_list = []
+  
+    s = s.split()
+    for w in s:
+        if w not in cache:
+            cache[w] = 1
+            new_list.append(w)
+    #return new_list
+    return " ".join(new_list)
+        
 
 
 
